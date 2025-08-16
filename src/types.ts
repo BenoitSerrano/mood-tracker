b610c88569a2d784f4f975e1c8b3003620845c41
Benoit Serrano <benoit.serrano10@gmail.com>
2025-08-16 23:17:53 +0200

    use new palette of color

--- src/types.ts
+++ src/types.ts
@@ -29,24 +29,24 @@ type minorEmotionMappingType = { grade: number; color: string; label: string };
 
 const emotionMapping: Record<majorEmotionType, minorEmotionMappingType[]> = {
     happiness: [
-        { grade: 0, color: '#D4EDB0', label: 'Content' },
-        { grade: 1, color: '#B2E672', label: 'Cheerful' },
-        { grade: 2, color: '#FFD93B', label: 'Happy' },
+        { grade: 0, color: '#efd18e', label: 'Content' },
+        { grade: 1, color: '#f3bc41', label: 'Cheerful' },
+        { grade: 2, color: '#f3ab1d', label: 'Happy' },
     ],
     sadness: [
-        { grade: 0, color: '#A3C4DC', label: 'Down' },
-        { grade: 1, color: '#6DA8D6', label: 'Sad' },
-        { grade: 2, color: '#41729F', label: 'Depressed' },
+        { grade: 0, color: '#71abae', label: 'Down' },
+        { grade: 1, color: '#31828c', label: 'Sad' },
+        { grade: 2, color: '#225a65', label: 'Depressed' },
     ],
     anxiety: [
-        { grade: 1, color: '#F88379', label: 'Uncomfortable' },
-        { grade: 2, color: '#E74C3C', label: 'Anxious' },
-        { grade: 3, color: '#C0392B', label: 'Panicked' },
+        { grade: 1, color: '#bdcaad', label: 'Uncomfortable' },
+        { grade: 2, color: '#98aa7e', label: 'Anxious' },
+        { grade: 3, color: '#707f54', label: 'Panicked' },
     ],
     fury: [
-        { grade: 1, color: '#FF5733', label: 'Upset' },
-        { grade: 2, color: '#C70039', label: 'Frustrated' },
-        { grade: 3, color: '#900C3F', label: 'Angry' },
+        { grade: 1, color: '#df9789', label: 'Upset' },
+        { grade: 2, color: '#db6a5a', label: 'Frustrated' },
+        { grade: 3, color: '#d6432f', label: 'Angry' },
     ],
 };
 
